c8c08ef100c0af8764596858e864478a96a2d607
FlaviusAB <31958193+FlaviusAB@users.noreply.github.com>
2022-05-23 13:44:56 +0200

    cleaned version

--- Client/Services/DbAccess.cs
+++ Client/Services/DbAccess.cs
@@ -8,30 +8,21 @@ namespace Client.Services;
 
 public interface IDbAccess
 {
-    Task<bool> CheckNameByInput(string username);
+  
     Task<string> RegisterUser(User user);
 }
 
 public class DbAccess : IDbAccess
 {
     private HttpClient _httpClient = new HttpClient();
-
-    public async Task<bool> CheckNameByInput(string username)
-    {
-        using var client = new HttpClient();
-        var content = await client.GetStringAsync("http://localhost:7071/api/user/" + username);
-
-        Console.WriteLine("HEREEEEEEEEEEEEEEEEE "+content);
-        return true;
-    }
+    
     public async Task<string> RegisterUser(User user)
     {
         var responseMsg = "failed";
         using var client = new HttpClient();
-        // var content = await client.GetStringAsync("http://localhost:7071/api/user/" + user.username);
-        string content = "false";
-
-        Console.WriteLine("COOOOOOO"+content);
+         
+        string content = await client.GetStringAsync("http://localhost:7071/api/user/" + user.username);
+        
         if (content.Equals("true"))
         {
             responseMsg = "username taken";
@@ -39,15 +30,12 @@ public class DbAccess : IDbAccess
         else
         {
             
-            
-            Console.WriteLine("1"+user.username);
             string message = JsonSerializer.Serialize(user);
             
             byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
             var msg = new ByteArrayContent(messageBytes);
             var response = await _httpClient.PostAsync("http://localhost:7071/api/signup", msg);
             
-            Console.WriteLine("2"+response);
             if (response.IsSuccessStatusCode)
             {
                 responseMsg = "user successfully registered";
