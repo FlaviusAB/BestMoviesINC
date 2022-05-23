49f33be5c67ff22ad4e0992d38e039bc9aaca286
Faustas V <55922917+prOcenTas@users.noreply.github.com>
2022-05-23 19:03:41 +0200

    signup

--- Api/UserController.cs
+++ Api/UserController.cs
@@ -29,8 +29,9 @@ namespace Api
                     conn.Open();  
                     if(!String.IsNullOrEmpty(input.username))  
                     {  
-                        Console.WriteLine(input.username +"djksvnskjdb");
-                        var query = $"INSERT INTO [login] (username,password,accessType) VALUES('{input.username}', '{input.password}' , '{input.accessType}')";  
+                        Console.WriteLine(input.email +" EMAIL");
+                        Console.WriteLine(input.username +" USERNAME");
+                        var query = $"INSERT INTO [login] (username,password,email,accessType) VALUES('{input.username}', '{input.password}' , '{input.email}' , '{input.accessType}')";  
                         SqlCommand command = new SqlCommand(query, conn);  
                         command.ExecuteNonQuery();  
                     }  
--- Client/Models/User.cs
+++ Client/Models/User.cs
@@ -5,13 +5,14 @@ public class User
     
     public string username { get; set; }
     public string password { get; set; }
-
+    public string email { get; set; }
     public string accessType { get; set; }
 
-    public User(string username, string password, string accessType)
+    public User(string username, string password, string email, string accessType)
     {
         this.username = username;
         this.password = password;
+        this.email = email;
         this.accessType = accessType;
     }
 }
