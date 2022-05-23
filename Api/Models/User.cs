aeea08701490e636b598b8dc3d6b2e3004510ed6
FlaviusAB <31958193+FlaviusAB@users.noreply.github.com>
2022-05-23 19:08:25 +0200

    auth files added

--- Api/Models/User.cs
+++ Api/Models/User.cs
@@ -7,5 +7,13 @@ public class User
     public string password { get; set; }
     public string accessType { get; set; }
     
-    
+    public string email { get; set; }
+
+    public User(string username, string password, string accessType, string email)
+    {
+        this.username = username;
+        this.password = password;
+        this.accessType = accessType;
+        this.email = email;
+    }
 }
--- Client/Models/User.cs
+++ Client/Models/User.cs
@@ -7,11 +7,14 @@ public class User
     public string password { get; set; }
 
     public string accessType { get; set; }
+    
+    public string email { get; set; }
 
-    public User(string username, string password, string accessType)
+    public User(string username, string password, string accessType, string email)
     {
         this.username = username;
         this.password = password;
         this.accessType = accessType;
+        this.email = email;
     }
 }
