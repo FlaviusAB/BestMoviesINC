d2f5c780637e91eabbb2520f9112af0f305ee1e5
Faustas V <55922917+prOcenTas@users.noreply.github.com>
2022-05-24 17:07:02 +0200

    localstorage

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,6 +6,7 @@ using Client.Authentication;
 using Client.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -18,5 +19,6 @@ builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
